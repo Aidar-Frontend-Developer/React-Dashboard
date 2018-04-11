cc71c12016de7c32ff686c9be95f58b88b12dc2a
Aidar Habibullin <zoomme.kzn@gmail.com>
2018-04-11 17:38:27 +0300

    Add navigation component

--- src/components/app.js
+++ src/components/app.js
@@ -1,36 +1,33 @@
 import React from "react";
-import { Layout, Menu, Breadcrumb } from "antd";
+import { Layout, Menu } from "antd";
+import Navigation from "./navigation";
 import "./app.css";
 
 const { Header, Content, Footer } = Layout;
 
 export const App = () => (
-  <Layout className="layout">
-    <Header>
-      <div className="logo" />
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        defaultSelectedKeys={["2"]}
-        style={{ lineHeight: "64px" }}
-      >
-        <Menu.Item key="1">nav 1</Menu.Item>
-        <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>
-      </Menu>
-    </Header>
-    <Content style={{ padding: "0 50px" }}>
-      <Breadcrumb style={{ margin: "16px 0" }}>
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>List</Breadcrumb.Item>
-        <Breadcrumb.Item>App</Breadcrumb.Item>
-      </Breadcrumb>
-      <div style={{ background: "#fff", padding: 24, minHeight: 280 }}>
-        Content
-      </div>
-    </Content>
-    <Footer style={{ textAlign: "center" }}>
-      Ant Design ©2016 Created by Ant UED
-    </Footer>
-  </Layout>
+    <Layout className="layout">
+        <Header>
+            <div className="logo" />
+            <Menu
+                theme="dark"
+                mode="horizontal"
+                defaultSelectedKeys={["2"]}
+                style={{ lineHeight: "64px" }}
+            >
+                <Menu.Item key="1">nav 1</Menu.Item>
+                <Menu.Item key="2">nav 2</Menu.Item>
+                <Menu.Item key="3">nav 3</Menu.Item>
+            </Menu>
+        </Header>
+        <Content className="content">
+            <Navigation />
+            <div style={{ background: "#fff", padding: 24, minHeight: 280 }}>
+                Content
+            </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+            Ant Design ©2016 Created by Ant UED
+        </Footer>
+    </Layout>
 );
